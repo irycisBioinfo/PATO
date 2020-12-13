04723d4573d9f3667c5f920c6d3f29b2a822a7c5
val <val@cuda.irycis.org>
2020-12-13 22:10:49 +0100

    developing core_by_mapping

--- R/core_by_mapping.R
+++ R/core_by_mapping.R
@@ -12,8 +12,9 @@ core_by_mapping <- function(file_list, n_core, ref, type)
 {
   if(grepl('linux',Sys.getenv("R_PLATFORM"))) ## Linux
   {
-    gsaPath = system.file("GSAlign",package = "pato")
-    bwtPath = system.file("bwt_index",package = "pato")
+    minimap2 = system.file("minimap2",package = "pato")
+    k8 = system.file("k8",package = "pato")
+    paftools = system.file("paftools.js", package="pato")
 
   }else if(grepl('apple',Sys.getenv("R_PLATFORM"))){ ##MacOS
     ### Need to compile
@@ -36,11 +37,14 @@ core_by_mapping <- function(file_list, n_core, ref, type)
     file_list <- file_list %>% as_tibble() %>% rename(File = 1)
   }
 
-  folderName = paste(getwd(),"/",md5(paste(file_list$File, sep = "",collapse = "")),"_gsa",sep = "",collapse = "")
+  folderName = paste(getwd(),"/",md5(paste(file_list$File, sep = "",collapse = "")),"_mmi",sep = "",collapse = "")
 
   if(!dir.exists(folderName))
   {
-    dir.create(folderName,)
+    dir.create(folderName)
+  }else{
+    system(paste("rm -r ",folderName))
+    dir.create(folderName)
   }
 
   if(missing(ref))
@@ -49,11 +53,6 @@ core_by_mapping <- function(file_list, n_core, ref, type)
   }
   print(paste("Reference: ",ref,sep = "",collapse = ""))
 
-  #origin_path = getwd()
-  #setwd(folderName)
-  #on.exit(setwd(origin_path))
-
-  system(paste(bwtPath," ",ref," ",folderName,"/reference.bwt", collapse = "", sep = ""))
 
 
 
@@ -65,25 +64,85 @@ core_by_mapping <- function(file_list, n_core, ref, type)
   options(warn = -1)
 
 
+  print("Indexing reference genome")
+  system(paste(minimap2," -d ref.mmi ",ref, sep = "", collapse = ""))
+
+  print("Aligning genomes")
   foreach (i = file_list$File) %dopar%{
 
-    system(paste(gsaPath," -one -i ",folderName,"/reference.bwt -q ",i," -o ",folderName,"/",basename(i),".output"," -t 2", sep = "", collapse = ""),ignore.stdout = T, ignore.stderr = T)
 
+    system(paste(minimap2," -cx asm20 -t 2 --cs ref.mmi ",i," > ",folderName,"/",basename(i),".paf",collapse = "", sep = ""), ignore.stderr = T)
+    system(paste(k8," ",paftools," call ",folderName,"/",basename(i),".paf"," > ",folderName,"/",basename(i),".vcf",collapse = "",sep = ""),ignore.stderr = T)
+    system(paste(k8," ",paftools," splice2bed ",folderName,"/",basename(i),".paf"," > ",folderName,"/",basename(i),".bed",collapse = "",sep = ""),ignore.stderr = T)
   }
   stopCluster(cl)
 
   options(warn = 0)
 
-  mafs <- dir(folderName, pattern = ".maf", full.names = T)
+  vcfs <- dir(folderName, pattern = ".vcf", full.names = T)
+  beds <- dir(folderName, pattern = ".bed", full.names = T)
+
+  system(
+     paste(
+       "bedtools intersect -a ",
+       beds[1],
+       " -b ",
+       paste(beds[-1], sep = " ", collapse = " "),
+       " | sort -k1,1 -k2,2n | bedtools merge > final.bed",
+       sep = "",
+       collapse = ""
+     )
+   )
+  print("Reading Bed Files")
+   bed <- read.table("final.bed")
+   colnames(bed) <- c("CHROM","START","END")
 
 
+   positions <- data.frame()
+   for(i in 1:nrow(bed))
+   {
+     positions <- bind_rows(positions, data.frame(POS = seq(bed$START[i]:bed$END[i]), CHROM = bed$CHROM[i], stringsAsFactors = F))
+   }
 
-  for(i in 1:length(mafs))
+  print("Reading VCF files")
+  vcfs <- vcfs[!grepl(basename(ref),vcfs)]
+  vcf_table <- data.frame()
+  for(i in 1:length(vcfs))
   {
-    maf_to_data.frame(mafs[i])
+    tmp <- fread(sep = "\t",header = F, stringsAsFactors = F,cmd = paste("grep 'V' ",vcfs[i], sep = "",collapse = ))
+    tmp$sample = basename(vcfs[i])
+    vcf_table = bind_rows(vcf_table,tmp)
   }
-  beds = gsub(".maf",".bed",mafs)
-  system(paste("bedtools intersect -a ",beds[1]," -b ",paste(beds[-1],sep = " ",collapse = " ")," >final.bed",sep = "",collapse = ""))
-  result = read.table("final.bed")
-  return(result)
+  colnames(vcf_table) <- c("type","CHROM","POS","end","query_depth","mapping_quality","REF","ALT","query_name","query_start","query_end","query_orientation","Sample")
+  vcf_table <- vcf_table %>% as_tibble()
+
+
+  result <- semi_join(vcf_table,positions)
+  result <- result %>%
+    filter(str_length(REF) ==1)%>%
+    filter(str_length(ALT) ==1)%>%
+    filter(REF != "-")%>%
+    filter(ALT != "-")%>%
+    select(CHROM,POS,REF,Sample,ALT) %>%
+    distinct() %>%
+    group_by(CHROM,REF,POS,Sample) %>%
+    mutate(Hplot = n()) %>%
+    filter(Hplot ==1)%>%
+    select(-Hplot) %>%
+    spread(Sample,ALT) %>%
+    ungroup()
+
+  for(i in 4:ncol(result))
+  {
+    result[[i]] <- coalesce(result[[i]],result$REF)
+  }
+  RefName = basename(ref)
+  colnames(result) = gsub("REF",RefName,colnames(result))
+  result = result %>% ungroup() %>% select(-CHROM, -POS) %>% t() %>% as.data.frame() %>% rownames_to_column("Genomes") %>% unite(Seq,-Genomes,sep = "")
+
+  return(list(aligment = result,vcf= vcf_table,bed = bed,path = folderName))
+  #return(list(vcf= vcf_table,pos = positions))
+
+
+
 }
