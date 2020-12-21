60065cf9f1a0365307a3f11f03b884b1353ec96c
valflanza <val@cuda.irycis.org>
2020-12-21 11:14:33 +0100

    fix DESCRIPTION file

---
+++ vignettes/vignette2.R
@@ -0,0 +1,26 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----eval=FALSE, include=TRUE-------------------------------------------------
+#  install.packages("devtools")
+
+## ----eval=FALSE, include=TRUE-------------------------------------------------
+#  setRepositories()
+#  ## Select options 1 (CRAN) and 2 (Bioconductor Software)
+
+## ----eval=FALSE, include=TRUE-------------------------------------------------
+#  devtools::install_github("https://github.com/irycisBioinfo/PATO.git", build_vignettes = TRUE)
+
+## ----eval=FALSE, include=TRUE-------------------------------------------------
+#  install.packages("devtools")
+
+## ----eval=FALSE, include=TRUE-------------------------------------------------
+#  setRepositories()
+#  ## Select options 1 (CRAN) and 2 (Bioconductor Software)
+
+## ----eval=FALSE, include=TRUE-------------------------------------------------
+#  devtools::install_github("https://github.com/irycisBioinfo/PATO.git", build_vignettes = TRUE)
+
